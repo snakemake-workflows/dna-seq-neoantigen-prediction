c390783e0ae2a7abe58ba99017423d47a28a6144
David Laehnemann <david.laehnemann@hhu.de>
2022-01-19 17:51:01 +0100

    fix render_scenario after changes to wildcards, samples.tsv and units.tsv

--- workflow/scripts/render-scenario.py
+++ workflow/scripts/render-scenario.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 with open(snakemake.input[0]) as template, open(snakemake.output[0], "w") as out:
     samples = snakemake.params.samples
+    group = samples.loc[samples["sample_name"] == snakemake.wildcards.cancer_sample, "group"]
     out.write(Template(template.read()).render(
-        samples=samples[(samples["sample"] == snakemake.wildcards.pair) | (samples["sample"] == samples.loc[snakemake.wildcards.pair, "matched_normal"])]
+        samples=samples[samples["group"] == group]
     ))
