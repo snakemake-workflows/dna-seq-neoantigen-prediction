dfd5760a87bc660a02ffcdc76dacdb913179d12b
dlaehnemann <david.laehnemann@hhu.de>
2022-09-01 15:56:45 +0000

    initial version of adjust_microphaser_output_for_neo_fox.py

--- workflow/scripts/adjust_microphaser_output_for_neo_fox.py
+++ workflow/scripts/adjust_microphaser_output_for_neo_fox.py
@@ -4,4 +4,17 @@ sys.stderr = open(snakemake.log[0], "w")
 
 import polars as pl
 
-candidates = pl.read_tsv(snakemake.input.candidates, sep="\t")
+columns_mapping = {
+    "gene_name": "gene",
+    "normal_peptide": "mutation.wildTypeXmer",
+    "tumor_peptide": "mutation.mutatedXmer",
+    "freq": "dnaVariantAlleleFrequency",
+}
+
+candidates = (
+    pl.read_tsv(snakemake.input.microphaser, sep="\t", quote="")
+    .rename(columns_mapping)
+    .with_column(pl.lit(snakemake.wildcards.group).alias("patientIdentifier"))
+)
+
+candidates.write_csv(snakemake.output.neo_fox, sep="\t", quote="")
